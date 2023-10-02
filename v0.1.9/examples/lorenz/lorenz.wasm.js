70635710b50ae7aa68dd273eb33f8c1c51fd6e25
Documenter.jl <documenter@juliadocs.github.io>
2023-10-02 15:46:26 +0000

    build based on 8b2bd2c

---
+++ v0.1.9/examples/lorenz/lorenz.wasm.js
@@ -0,0 +1,8 @@
+var jsexports = { js: {} };
+jsexports['js']['(id, data, layout, config) => Plotly.newPlot(id, data, layout, config)'] = (id, data, layout, config) => Plotly.newPlot(id, data, layout, config);
+jsexports['js']['(v, i, x) => v[i] = x'] = (v, i, x) => v[i] = x;
+jsexports['js']['x => console.log(x)'] = x => console.log(x);
+jsexports['js']['x => mdpad[x]'] = x => mdpad[x];
+jsexports['js']['n => Array(n)'] = n => Array(n);
+jsexports['js']['n => new Float64Array(n)'] = n => new Float64Array(n);
+jsexports['js']['() => ({})'] = () => ({});
