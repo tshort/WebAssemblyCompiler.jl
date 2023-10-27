07bc8f8508e7c94f03fa44fb6adc1b807602ea8e
Tom Short <tshort.rlists@gmail.com>
2023-10-26 22:31:35 -0400

    Try another test

--- test/local.jl
+++ test/local.jl
@@ -6,19 +6,13 @@ const WebAssemblyCompiler._DEBUG_ = true
 # const WebAssemblyCompiler._DEBUG_ = false
    
    
-    @noinline fargs5a(a, args...) = args
-    function fargs5(x)
-        tup = fargs5a(x, 1, 1.1)
-        return x * tup[2]
-    end
-    compile((fargs5, Float64,); filepath = "tmp/fargs5.wasm", validate = true)
 
  
-    # function fdict1(x)
-    #     d = Dict{Int, Float64}((1 => 10., 2 => 20., 3 => 30.))
-    #     get(d, 2, -1.0) + x
-    # end
-    # compile((fdict1, Float64,); filepath = "tmp/fdict1.wasm")
+    function fdict2(x)
+        d = Dict{String, String}(("1" => "10.", "2" => "20.", "3" => "30."))
+        get(d, 2, -1.0) + x
+    end
+    compile((fdict2, Float64,); filepath = "tmp/fdict2.wasm")
 
     # function fhash2(x)
     #     z = Base.hashindex("jhkjh", 3)
