6b39bac7670df1fc40ee807cc4d12759b0e4c285
Documenter.jl <documenter@juliadocs.github.io>
2023-11-01 21:52:57 +0000

    build based on fdb7bbc

---
+++ v0.1.14/examples/observables/observables.wasm.js
@@ -0,0 +1,10 @@
+var jsexports = { js: {} };
+jsexports['js']['(v, i, x) => v[i] = x'] = (v, i, x) => v[i] = x;
+jsexports['js']['(x) => (new TextDecoder()).decode(x)'] = (x) => (new TextDecoder()).decode(x);
+jsexports['js']['(x,sep) => x.join(sep)'] = (x,sep) => x.join(sep);
+jsexports['js']['(x, str) => x.innerHTML = str'] = (x, str) => x.innerHTML = str;
+jsexports['js']['(x) => console.log(x)'] = (x) => console.log(x);
+jsexports['js']['n => Array(n)'] = n => Array(n);
+jsexports['js']['(x) => document.getElementById(x)'] = (x) => document.getElementById(x);
+jsexports['js']['(x) => Math.sin(x)'] = (x) => Math.sin(x);
+jsexports['js']['n => new Uint8Array(n)'] = n => new Uint8Array(n);
